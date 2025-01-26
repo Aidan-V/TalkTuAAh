791acbed5cd1847684564f115723c437e3598e85
Leo dos Remedios <leondosr@outlook.com>
2025-01-25 18:10:19 -0600

    added firebase auth

---
+++ firebase.config.ts
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAoHlSvP-bhO0c-cs7hsjsWk7I_IGhDdR8",
+  authDomain: "talktuaah.firebaseapp.com",
+  projectId: "talktuaah",
+  storageBucket: "talktuaah.firebasestorage.app",
+  messagingSenderId: "425962632045",
+  appId: "1:425962632045:web:8c2d9a1bb703e8dc182064"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
